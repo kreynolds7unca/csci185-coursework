9c4813fa66f4f028f2133b1a93f9eee52405e7d7
Kris <kreynol7@unca.edu>
2022-10-05 14:38:29 -0400

    homework 3

--- homework/hw03/sketch.js
+++ homework/hw03/sketch.js
@@ -7,14 +7,70 @@ function setup() {
     // invoke drawCreature 5 times with the following arguments
     // (feel free to change the sizes, positions, and colors):
     
-    // drawCreature(92, 115, 85, '#5e6976', '#1b324d');
-    // drawCreature(487, 110, 101, '#bfdc65', '#abb880');
-    // drawCreature(454, 423, 141, '#aebb83', '#227876');
-    // drawCreature(333, 227, 99, '#94ba77', '#3f5364');
-    // drawCreature(117, 314, 91, '#648d8e', '#afc272');
+
+    drawCreature(100, 400, 200, faceColor="grey", eyeColor="black");
+    drawCreature(500, 400, 100, faceColor="purple", eyeColor="hotpink");
+    drawCreature(900, 400, 50, faceColor="yellow", eyeColor="blue");
     
     drawGrid(canvasWidth, canvasHeight);
 }
 
 // define your drawCreature function here:
+//faceColor = body (& ears)
+//eyeColor = eyes
+
+function drawCreature (centerX, centerY, size, faceColor="brown", eyeColor="black") {
+    //eyes & body
+    strokeWeight(0); //border 
+    fill(faceColor);
+    circle(centerX, centerY, size); // previously: 100, 400, 50
+    fill(eyeColor);
+    let sf = size/ 6; 
+    circle(centerX-sf, centerY-sf, sf/2);  // left eye - previously: 92, 392, 8
+    circle(centerX+sf, centerY-sf, sf/2); // right eye - previously: 108, 392, 8
+   
+    //nose
+    fill('white');
+    strokeWeight(0);
+    let noseWidth = sf*2;
+    let noseHeight = sf*1.5;
+    ellipse(centerX, centerY, noseWidth, noseHeight); 
+    fill('grey');
+    let size1 = sf/3.5;
+    let size2 = sf/3.5;
+    let nosedotsPosition = sf/2; 
+    ellipse(centerX-nosedotsPosition, centerY, size1, size2); 
+    ellipse(centerX+nosedotsPosition, centerY, size1, size2);
+    
+    //ears
+    strokeWeight(1);
+    stroke ('black');
+    // left ear
+    push();
+    translate(centerX-sf*2.5, centerY-sf); //was 15, 360
+    rotate(-1);
+    fill(faceColor);
+    ellipse(0, 0, size*(2/3), size/3.5);
+    //was ellipse(0, 0, 125, 50);
+    pop();
+    // right ear
+    push();
+    translate(centerX+sf*2.5, centerY-sf); // was 185, 360
+    rotate(1);
+    fill(faceColor);
+    ellipse(0, 0, size*(2/3), size/3.5);
+    pop();
+  
+
+//belly curve
+    strokeWeight(size/ 25);
+    fill('white')
+    strokeWeight(0);
+    curve (
+        centerX+size, centerY+size*1.75,  // control point
+        centerX-sf*1.5, centerY+sf*2.2, //start point
+        centerX+sf*1.5, centerY+sf*2.2, //end point
+        centerX-size, centerY+size*1.75, //control point
+    );
+}
 
--- tutorials/tutorial07/sketch.js
+++ tutorials/tutorial07/sketch.js
@@ -4,16 +4,28 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
 
     // Exercise 1:
-    drawCircle();
+    drawCircle(100, 100, 80, 'hotpink'); //x, y, diameter, color
+    drawCircle(200, 100, 40, 'navy');
+    drawCircle(300, 100, 80, 'teal');
+    drawCircle(400, 100, 40, 'hotpink');
 
     // Exercise 2: 
-    drawOval();
+    drawOval(100, 200, 100, 62.5, 'teal');
+    drawOval(200, 200, 62.5, 100, 'hotpink');
+    drawOval(300, 200, 100, 62.5, 'navy');
+    drawOval(400, 200, 62.5, 100, 'teal');
 
     // Exercise 3:
-    drawBullseye();
+    drawBullseye(100, 300, 100);
+    drawBullseye(200, 300, 50);
+    drawBullseye(300, 300, 100);
+    drawBullseye(400, 300, 50);
 
     // Exercise 4:
-    drawFace();
+    drawFace(100, 400, 50);
+    drawFace(200, 400, 100);
+    drawFace(300, 400, 50);
+    drawFace(400, 400, 100);
 
     drawGrid(canvasWidth, canvasHeight);
 }
@@ -21,38 +33,72 @@ function setup() {
 
 // modify this function so that it accepts and honors
 // the following parameters: centerX, centerY, size, and fillColor
-function drawCircle() {
-    fill('hotpink');
-    circle(100, 100, 100);
+//drawCircle(300, 400, 40, 'yellow');
+
+function drawCircle(centerX, centerY, size, fillColor) {
+    fill(fillColor);
+    circle(centerX, centerY, size);
 }
 
 // modify this function so that it accepts and honors
 // the following parameters: centerX, centerY, sizeX, sizeY, and fillColor
-function drawOval() {
-    fill('teal');
-    ellipse(100, 200, 100, 62.5);
+//drawOval(100, 200, 100, 62.5, 'teal');
+
+function drawOval(centerX, centerY, sizeX, sizeY, fillColor) {
+    fill(fillColor);
+    ellipse(centerX, centerY, sizeX, sizeY);
 }
 
 
 // modify this function so that it accepts and honors
 // the following 3 parameters: centerX, centerY, and size
-function drawBullseye() {
-    fill('teal');
-    circle(100, 300, 100);
-    fill('navy');
-    circle(100, 300, 75);
-    fill('teal');
-    circle(100, 300, 50);
-    fill('navy');
-    circle(100, 300, 25);
+//size is based on percentages, going down by 25% each time
+//color added for options if user doesn't add their own colors 
+
+function drawBullseye(centerX, centerY, size, fillColor1='teal', fillColor2='navy') {
+    fill(fillColor1);
+    circle(centerX, centerY, size);
+    fill(fillColor2);
+    circle(centerX, centerY, size * .75);
+    fill(fillColor1);
+    circle(centerX, centerY, size * .5);
+    fill(fillColor2);
+    circle(centerX, centerY, size * .25);
 }
 
 // modify this function so that it accepts and honors
 // the following 3 parameters: centerX, centerY, and size
-function drawFace() {
-    fill('white');
-    circle(100, 400, 50);
+// scale factor: was centerX-8, centerY-8, 8, but scaling, but proportion is off.
+// How much 8 can go into 50. Can have multiple scale factors in one function and/or shape. 
+
+function drawFace(centerX, centerY, size, faceColor="lavender") {
+    strokeWeight(1); //border 
+    fill(faceColor);
+    circle(centerX, centerY, size); // previously: 100, 400, 50
     fill('black');
-    circle(92, 392, 8);
-    circle(108, 392, 8);
+    let sf = size/ 6; 
+    circle(centerX-sf, centerY-sf, sf);  // left eye - previously: 92, 392, 8
+    circle(centerX+sf, centerY-sf, sf); // right eye - previously: 108, 392, 8
+
+// mouth - start with flat line with 2 coordinates.
+    // strokeWeight(size/ 25); //mouth   
+    // line (
+    //     centerX-sf*2, // x1
+    //     centerY+sf, // y1
+    //     centerX+sf*2, //x2
+    //     centerY+sf, //y2
+    // ); 
+
+// mouth curve
+    strokeWeight(size/ 25);
+    noFill()
+    stroke('black');
+    curve (
+        centerX-sf*2, centerY-size,  // control point
+        centerX-sf*2, centerY+sf, //start point
+        centerX+sf*2, centerY+sf, //end point
+        centerX+sf*2, centerY-size, //control point
+    ); 
+
 }
+

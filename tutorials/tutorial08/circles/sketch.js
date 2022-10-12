53c26efd2bb656d0af2fb157c2572c431f145381
Kris <kreynol7@unca.edu>
2022-10-12 16:39:10 -0400

    lecture 14 & tutorial 8_02

--- tutorials/tutorial08/circles/sketch.js
+++ tutorials/tutorial08/circles/sketch.js
@@ -5,11 +5,37 @@ function setup() {
 
     // fill('red');
     noFill();
-    circle(100, 200, 50);
-    circle(100, 250, 50);
-    circle(100, 300, 50);
-    circle(100, 350, 50);
-    circle(100, 400, 50);
+    // question 1: what do we want to repeat? Draw a circle. 
+    // question 2: how long do we want to repeat? 5
+    // question 3: what will change each time? The Y position by 50. 
+   
+   let y = 0;
+    while(y <= canvasHeight) {
+        fill ('lavender');
+        circle(y + 100, y, 50);
+        fill ('grey');
+        circle(y + 200, y, 50);
+        fill ('lavender');
+        circle(y+ 300, y, 50);
+        fill ('grey');
+        circle(y+ 400, y, 50);
+        y += 50; 
+    }
+
+    // let x = canvasWidth / 2;
+    // let y = canvasHeight / 2; 
+    // let size = canvasWidth; 
+    // let fillColor = 'lavender';
+    // while (size >= 25) {
+    //     if (fillColor == 'lavender') {
+    //         fillColor = 'grey'
+    //     } else {
+    //         fillColor = 'lavender';
+    //     }
+    //     fill(fillColor); 
+    //     circle (x, y, size); 
+    //     size -= 25; 
+    // }
 
     drawGrid(canvasWidth, canvasHeight);
 }
